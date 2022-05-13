ec0b12d45b429391e3d9aa0c46c0a9493da803d5
longduy0810 <96179675+longduy0810@users.noreply.github.com>
2022-05-13 19:45:06 +0700

    Edit Entities for StudentDetail and Transcript

--- src/main/java/com/app/studentmanagement/entities/StudentDetail.java
+++ src/main/java/com/app/studentmanagement/entities/StudentDetail.java
@@ -16,7 +16,7 @@ import lombok.NoArgsConstructor;
 @Data
 @Entity
 @Table(name="student_detail")
-@NamedQuery(name="StudentDetail.findAll", query="SELECT s FROM StudentDetail sd")
+@NamedQuery(name="StudentDetail.findAll", query="SELECT sd FROM StudentDetail sd")
 public class StudentDetail implements Serializable {
     private static final long serialVersionUID = 1L;
 
