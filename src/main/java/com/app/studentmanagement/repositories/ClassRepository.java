d9d7b49b9df662f8a2feab2401e423f5fa5562d6
Thang0812 <84112181+Thang0812@users.noreply.github.com>
2022-05-15 12:14:06 +0700

    Add Repository: Class

---
+++ src/main/java/com/app/studentmanagement/repositories/ClassRepository.java
@@ -0,0 +1,11 @@
+package com.app.studentmanagement.repositories;
+
+import com.app.studentmanagement.entities.Class;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClassRepository extends JpaRepository<Class,Integer>{
+
+}
