745886e2d5f830f8eca677c6d43eeb179cc86124
Thang0812 <84112181+Thang0812@users.noreply.github.com>
2022-05-15 12:24:04 +0700

    Add Implement: Class

---
+++ src/main/java/com/app/studentmanagement/services/impl/CLassServiceImpl.java
@@ -0,0 +1,49 @@
+package com.app.studentmanagement.services.impl;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import com.app.studentmanagement.entities.Class;
+import com.app.studentmanagement.repositories.ClassRepository;
+import com.app.studentmanagement.services.ClassService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class ClassServiceImpl implements ClassService{
+
+    @Autowired
+    ClassRepository ClassRepository;
+
+    @Override
+    public Page<Class> findAll(Pageable pageable) {
+        return ClassRepository.findAll(pageable);
+    }
+
+    @Override
+    public List<Class> findAll() {
+        return ClassRepository.findAll();
+    }
+
+    @Override
+    public Class findById(int id) {
+        return ClassRepository.findById(id).get();
+    }
+
+    @Override
+    public void deleteById(int id) {
+        ClassRepository.deleteById(id);
+
+    }
+
+    @Override
+    public Class save(Class _class) {
+        return ClassRepository.save(_class);
+    }
+
+}
